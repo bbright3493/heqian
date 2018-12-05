84d002948983c8b714cd5ebe9d4e837e8bdcdda2
bbright3493 <44704708@qq.com>
2018-12-05 11:13:21 +0800

    数据库调整到服务器

--- olreg/models.py
+++ olreg/models.py
@@ -187,7 +187,8 @@ class HosptialIntroduce(models.Model):
     medicinal_intr = models.CharField(max_length=500, verbose_name='药材介绍', default='')
     server_intr = models.CharField(max_length=500, verbose_name='服务介绍',default='')
     work_time = models.CharField(max_length=300, verbose_name='营业时间', default='',help_text='如 9:00---19:00')
-    contract_intr = models.CharField(max_length=300, verbose_name='联系电话', default='')
+    contract_intr = models.CharField(max_length=100, verbose_name='联系电话', default='')
+    work_phone = models.CharField(max_length=50, verbose_name='值班电话', default='')
     #image = models.ImageField(upload_to="hosptial/%Y/%m", verbose_name=u"介绍配图", max_length=200, default='')
 
     def __str__(self):
